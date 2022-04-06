bfdd701b44881330c78b4aa2885bd7dd1707c0fc
hub33k <hub33k@yahoo.com>
2022-04-06 20:13:59 +0200

    chore: prepare server files

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      envFilePath: ['.env', '.env.development', '.env.production', '.env.test'],
+      validationSchema: Joi.object({
+        // General
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(4000),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
