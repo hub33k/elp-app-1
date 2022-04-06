d9735b4843291beb0d27d23f1c7d7b3c1ebdff86
hub33k <hub33k@yahoo.com>
2022-04-06 20:29:53 +0200

    chore: add example common interface

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,8 +1,22 @@
 import type { NextPage } from 'next';
-import common from 'common';
+import common, { IHello } from 'common';
+
+class Hello implements IHello {
+  name: string;
+  range: number;
+
+  constructor(name: string, range: number) {
+    this.name = name;
+    this.range = range;
+  }
+}
 
 const HomePage: NextPage = () => {
   console.log(common);
+
+  const hello = new Hello('World', 30);
+  console.log(hello);
+
   return (
     <>
       <h1>Home page</h1>
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,5 @@
+export * from './interfaces';
+
 export default {
   common: true,
 };
